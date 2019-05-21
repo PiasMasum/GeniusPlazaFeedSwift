894d462610d6f8d2c3c78652e1f1669fb7d1ee6f
Pias Khan <piasmasum@gmail.com>
2019-05-22 05:06:16 +0600

    added mediaviewcontroller as root viewcontroller

---
+++ GeniusPlazaFeedSwift/MediaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MediaViewController.swift
+//  GeniusPlazaFeedSwift
+//
+//  Created by Pias khan on 5/22/19.
+//  Copyright © 2019 Pias khan. All rights reserved.
+//
+
+import UIKit
+
+class MediaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
